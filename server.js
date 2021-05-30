77128afe2f0e4a1a5ec3cbd76f83c6c15e12803f
David K <dkim@thomasnet.com>
2021-05-30 18:29:25 -0400

    Updated cors issue

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.port || 5000;
+
+// Allow cross-origin
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -12,5 +17,12 @@ app.use(
     graphiql: true,
   })
 );
+
+// Set static dir
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
